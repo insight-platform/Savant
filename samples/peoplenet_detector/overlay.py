ce3b095a7370a1f3ecbf027103b76f4ec45746e4
Oleg V. Abramov <abramov.o.v@gmail.com>
2023-03-13 19:01:29 +0700

    animation wip

--- samples/peoplenet_detector/overlay.py
+++ samples/peoplenet_detector/overlay.py
@@ -1,11 +1,36 @@
+import numpy as np
 import cv2
 
 from savant.deepstream.drawfunc import NvDsDrawFunc
 from savant.deepstream.meta.frame import NvDsFrameMeta
-from savant.meta.bbox import BBox
 from savant.utils.artist import Artist
-from savant.meta.constants import UNTRACKED_OBJECT_ID
-from savant.utils.artist import Position, Artist, COLOR
+from savant.utils.artist import Position, Artist
+
+
+def load_sprite(path:str, target_height:int) -> cv2.cuda.GpuMat:
+    sprite = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    sprite = cv2.cvtColor(sprite, cv2.COLOR_BGRA2RGBA)
+    resize_coeff = target_height / sprite.shape[0]
+    sprite = cv2.resize(sprite, dsize=None, fx=resize_coeff, fy=resize_coeff, interpolation=cv2.INTER_CUBIC)
+    return cv2.cuda.GpuMat(sprite)
+
+
+def get_font_scale(target_height_px: int, font_thickness:float=1, font_face:int = cv2.FONT_HERSHEY_SIMPLEX, sample_text='0123456789') -> float:
+    font_scale = 0.5
+    text_size, _ = cv2.getTextSize(
+        sample_text, font_face, font_scale, font_thickness
+    )
+    min_delta = abs(target_height_px - text_size[1])
+    for scale in np.linspace(0.6, 5, 45):
+        text_size, baseline = cv2.getTextSize(
+            sample_text, font_face, scale, font_thickness
+        )
+        delta = abs(target_height_px - text_size[1])
+        if delta < min_delta:
+            min_delta = delta
+            font_scale = scale
+            _ = baseline
+    return font_scale
 
 
 class Overlay(NvDsDrawFunc):
@@ -19,22 +44,81 @@ class Overlay(NvDsDrawFunc):
         self.bbox_border_width = 3
         self.overlay_height = 180
         self.logo_height = 120
-        
-        logo = cv2.imread(
-            '/opt/app/samples/peoplenet_detector/logo_insight.png',
-            cv2.IMREAD_UNCHANGED,
-        )
-        logo = cv2.cvtColor(logo, cv2.COLOR_BGRA2RGBA)
-        logo_resize_coeff = self.logo_height / logo.shape[0]
-        logo = cv2.resize(logo, dsize=None, fx=logo_resize_coeff, fy=logo_resize_coeff, interpolation=cv2.INTER_CUBIC)
-        self.logo = cv2.cuda.GpuMat(logo)
+        self.sprite_heigth = 120
+        self.letter_height = 85
 
-        self.logo_top = (self.overlay_height - self.logo_height) // 2
+        self.font_thickness = 5
+        self.font_face = cv2.FONT_HERSHEY_SIMPLEX
+        self.font_scale = get_font_scale(self.letter_height, self.font_thickness, self.font_face)
+
+        text_w = cv2.getTextSize(
+            '99', self.font_face, self.font_scale, self.font_thickness
+        )[0][0]
+        narrow_sep_w = 15
+        sep_w = 60
+
+        self.logo = load_sprite('/opt/app/samples/peoplenet_detector/logo_insight.png', self.logo_height)
+        self.green_sprite = load_sprite('/opt/app/samples/peoplenet_detector/animation/1/1_001.png', self.sprite_heigth)
+        self.blue_sprite = load_sprite('/opt/app/samples/peoplenet_detector/animation/2/2_001.png', self.sprite_heigth)
+
+        logo_w = self.logo.size()[0]
+        sprite_w = self.green_sprite.size()[0]
+
+        cum_left = 0
+        self.logo_pos = cum_left, (self.overlay_height - self.logo_height) // 2
+
+        cum_left += logo_w + sep_w
+        self.green_sprite_tl = cum_left, (self.overlay_height - self.sprite_heigth) // 2
+
+        cum_left += sprite_w + narrow_sep_w
+        self.green_text_tl = cum_left, (self.overlay_height - self.letter_height) // 2
+
+        cum_left += text_w + sep_w
+        self.blue_sprite_tl = cum_left, (self.overlay_height - self.sprite_heigth) // 2
+
+        cum_left += sprite_w + narrow_sep_w
+        self.blue_text_tl = cum_left, (self.overlay_height - self.letter_height) // 2
+
+        anim_fps = 10
+        self.anim_frames_n = 10
+        self.frame_period = 10**9 / anim_fps
+        self.current_frame_i = 1
+        self.ts = 0
+
 
     def draw_on_frame(self, frame_meta: NvDsFrameMeta, artist: Artist):
         """
         """
-        artist.add_overlay(self.logo, (0,self.logo_top))
+
+        artist.add_overlay(self.logo, self.logo_pos)
+        artist.add_overlay(self.green_sprite, self.green_sprite_tl)
+        artist.add_overlay(self.blue_sprite, self.blue_sprite_tl)
+        artist.add_text(
+            f'{self.current_frame_i}',
+            self.green_text_tl[0],
+            self.green_text_tl[1],
+            self.font_scale,
+            self.font_thickness,
+            (1,1,1),
+            padding=0,
+            anchor_point=Position.LEFT_TOP
+        )
+        artist.add_text(
+            f'{self.current_frame_i}',
+            self.blue_text_tl[0],
+            self.blue_text_tl[1],
+            self.font_scale,
+            self.font_thickness,
+            (1,1,1),
+            padding=0,
+            anchor_point=Position.LEFT_TOP
+        )
+        if frame_meta.pts - self.ts >= self.frame_period:
+            self.current_frame_i += 1
+            if self.current_frame_i > self.anim_frames_n:
+                self.current_frame_i = 1
+            self.ts = frame_meta.pts
+
         for obj_meta in frame_meta.objects:
             if obj_meta.is_primary:
                 continue
