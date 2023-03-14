00e3ac67bb65887f2c3f5cb570b70c645f54c137
Oleg V. Abramov <abramov.o.v@gmail.com>
2023-03-14 12:22:41 +0700

    added sprite animation

---
+++ samples/peoplenet_detector/animation.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import cv2
+from samples.peoplenet_detector.utils import load_sprite
+
+
+class Animation:
+    def __init__(self, sprites_dir: str, fps: int, sprite_height: int) -> None:
+
+        self.fps = fps
+        self.frame_period_ns = 10**9 // self.fps
+        self.sprites = [
+            load_sprite(str(path), sprite_height)
+            for path in sorted(Path(sprites_dir).glob('*.png'))
+        ]
+        self.current_sprite_idx = 0
+        self.last_change_pts = 0
+
+    @property
+    def width(self) -> int:
+        return self.sprites[0].size()[0]
+
+    def get_frame(self, pts: int) -> cv2.cuda.GpuMat:
+        sprite = self.sprites[self.current_sprite_idx]
+
+        if pts - self.last_change_pts >= self.frame_period_ns:
+            self.current_sprite_idx += 1
+            if self.current_sprite_idx >= len(self.sprites):
+                self.current_sprite_idx = 0
+            self.last_change_pts = pts
+
+        return sprite
--- samples/peoplenet_detector/overlay.py
+++ samples/peoplenet_detector/overlay.py
@@ -1,53 +1,11 @@
-from typing import Tuple
-import numpy as np
 import cv2
 
 from savant.deepstream.drawfunc import NvDsDrawFunc
 from savant.deepstream.meta.frame import NvDsFrameMeta, BBox
 from savant.utils.artist import Artist
 from savant.utils.artist import Position, Artist
-
-
-def load_sprite(path: str, target_height: int) -> cv2.cuda.GpuMat:
-    """Read from file, resize according to target height, move to GPU."""
-    sprite = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    sprite = cv2.cvtColor(sprite, cv2.COLOR_BGRA2RGBA)
-    resize_coeff = target_height / sprite.shape[0]
-    sprite = cv2.resize(
-        sprite,
-        dsize=None,
-        fx=resize_coeff,
-        fy=resize_coeff,
-        interpolation=cv2.INTER_CUBIC,
-    )
-    return cv2.cuda.GpuMat(sprite)
-
-
-def get_font_scale(
-    target_height_px: int,
-    font_thickness: float = 1,
-    font_face: int = cv2.FONT_HERSHEY_SIMPLEX,
-    sample_text: str = '0123456789',
-    font_scale_range: Tuple[float, float] = (0.1, 5),
-    font_scale_step: float = 0.1,
-) -> float:
-    """Find a font scale for OpenCV's text according to target text height in pixels."""
-
-    font_scale = font_scale_range[0]
-    text_size, _ = cv2.getTextSize(sample_text, font_face, font_scale, font_thickness)
-    min_delta = abs(target_height_px - text_size[1])
-
-    scale_range_start = font_scale + font_scale_step
-    for scale in np.arange(scale_range_start, font_scale_range[1], font_scale_step):
-        text_size, baseline = cv2.getTextSize(
-            sample_text, font_face, scale, font_thickness
-        )
-        delta = abs(target_height_px - text_size[1])
-        if delta < min_delta:
-            min_delta = delta
-            font_scale = scale
-            _ = baseline
-    return font_scale
+from samples.peoplenet_detector.animation import Animation
+from samples.peoplenet_detector.utils import load_sprite, get_font_scale
 
 
 class Overlay(NvDsDrawFunc):
@@ -76,19 +34,23 @@ class Overlay(NvDsDrawFunc):
         sep_w = 60
 
         self.logo = load_sprite(
-            '/opt/app/samples/peoplenet_detector/logo_insight.png', self.logo_height
+            '/opt/app/samples/peoplenet_detector/sprites/logo_insight.png',
+            self.logo_height,
         )
-        self.green_sprite = load_sprite(
-            '/opt/app/samples/peoplenet_detector/animation/1/1_001.png',
+
+        self.green_man = Animation(
+            '/opt/app/samples/peoplenet_detector/sprites/green_man/',
+            10,
             self.sprite_heigth,
         )
-        self.blue_sprite = load_sprite(
-            '/opt/app/samples/peoplenet_detector/animation/2/2_001.png',
+        self.blue_man = Animation(
+            '/opt/app/samples/peoplenet_detector/sprites/blue_man/',
+            10,
             self.sprite_heigth,
         )
 
         logo_w = self.logo.size()[0]
-        sprite_w = self.green_sprite.size()[0]
+        sprite_w = self.green_man.width
 
         cum_left = 0
         self.logo_pos = cum_left, (self.overlay_height - self.logo_height) // 2
@@ -104,12 +66,6 @@ class Overlay(NvDsDrawFunc):
 
         cum_left += sprite_w + narrow_sep_w
         self.blue_text_tl = cum_left, (self.overlay_height - self.letter_height) // 2
-
-        anim_fps = 10
-        self.anim_frames_n = 10
-        self.frame_period = 10**9 / anim_fps
-        self.current_frame_i = 1
-        self.ts = 0
 
     def draw_on_frame(self, frame_meta: NvDsFrameMeta, artist: Artist):
         """ """
@@ -129,10 +85,12 @@ class Overlay(NvDsDrawFunc):
             padding=0,
         )
         artist.add_overlay(self.logo, self.logo_pos)
-        artist.add_overlay(self.green_sprite, self.green_sprite_tl)
-        artist.add_overlay(self.blue_sprite, self.blue_sprite_tl)
+        artist.add_overlay(
+            self.green_man.get_frame(frame_meta.pts), self.green_sprite_tl
+        )
+        artist.add_overlay(self.blue_man.get_frame(frame_meta.pts), self.blue_sprite_tl)
         artist.add_text(
-            f'{self.current_frame_i}',
+            '7',
             self.green_text_tl[0],
             self.green_text_tl[1],
             self.font_scale,
@@ -142,7 +100,7 @@ class Overlay(NvDsDrawFunc):
             anchor_point=Position.LEFT_TOP,
         )
         artist.add_text(
-            f'{self.current_frame_i}',
+            '12',
             self.blue_text_tl[0],
             self.blue_text_tl[1],
             self.font_scale,
@@ -151,11 +109,6 @@ class Overlay(NvDsDrawFunc):
             padding=0,
             anchor_point=Position.LEFT_TOP,
         )
-        if frame_meta.pts - self.ts >= self.frame_period:
-            self.current_frame_i += 1
-            if self.current_frame_i > self.anim_frames_n:
-                self.current_frame_i = 1
-            self.ts = frame_meta.pts
 
         for obj_meta in frame_meta.objects:
             if obj_meta.is_primary:
