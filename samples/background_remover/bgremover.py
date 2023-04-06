333bb1d92dbf1a49b320bb37698707fc7dd02be9
bogoslovskiy_nn <bogoslovskiy_nn@bw-sw.com>
2023-04-06 15:15:22 +0700

    added background remover sample

---
+++ samples/background_remover/bgremover.py
@@ -0,0 +1,45 @@
+"""Background remover module."""
+from savant.gstreamer import Gst
+from savant.deepstream.meta.frame import NvDsFrameMeta
+from savant.deepstream.pyfunc import NvDsPyFuncPlugin
+from savant.utils.artist import Artist
+from savant.deepstream.opencv_utils import (
+    nvds_to_gpu_mat,
+)
+import cv2
+
+class BgRemover(NvDsPyFuncPlugin):
+    """Background remover pyfunc.
+
+    The class is designed to process video frame metadata and remove the background from the frame.
+    MOG2 method from openCV is used to remove background.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.stream = cv2.cuda.Stream_Null()
+        self.back_subtractors = {}
+
+    def on_source_eos(self, source_id: str):
+        """On source EOS event callback."""
+        self.back_subtractors.pop(source_id)
+
+    def process_frame(self, buffer: Gst.Buffer, frame_meta: NvDsFrameMeta):
+        """Process frame metadata.
+
+        :param buffer: Gstreamer buffer with this frame's data.
+        :param frame_meta: This frame's metadata.
+        """
+        pass
+        with nvds_to_gpu_mat(buffer, frame_meta.frame_meta) as frame_mat:
+            with Artist(frame_mat) as artist:
+                if frame_meta.source_id in self.back_subtractors:
+                    back_sub = self.back_subtractors[frame_meta.source_id]
+                else:
+                    back_sub = cv2.cuda.createBackgroundSubtractorMOG2()
+                    self.back_subtractors[frame_meta.source_id] = back_sub
+
+                cropped = cv2.cuda_GpuMat(frame_mat, (0, 0, int(frame_meta.roi.width), int(frame_meta.roi.height)))
+                cu_mat_fg = back_sub.apply(cropped, -1, self.stream)
+                res_image = cropped.copyTo(cu_mat_fg, self.stream)
+                artist.add_graphic(res_image, (int(frame_meta.roi.width), 0))
