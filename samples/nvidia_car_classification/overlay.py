da524ac9c20023a3ac291c98683cf3ea9fc72f55
Oleg V. Abramov <abramov.o.v@gmail.com>
2023-05-31 15:18:03 +0700

    Update car sample to use new custom drawing

--- samples/nvidia_car_classification/overlay.py
+++ samples/nvidia_car_classification/overlay.py
@@ -1,41 +1,25 @@
 """Draw func adds car classification models outputs: car color, car make, car type."""
 
 from savant.deepstream.drawfunc import NvDsDrawFunc
-from savant.deepstream.meta.frame import NvDsFrameMeta
-from savant.utils.artist import Position, Artist
+from savant.meta.object import ObjectMeta
 
 
 class Overlay(NvDsDrawFunc):
-    def draw_on_frame(self, frame_meta: NvDsFrameMeta, artist: Artist):
-        """DrawFunc's method where the drawing happens.
-        Use artist's methods to add custom graphics to the frame.
 
-        :param frame_meta: This frame's metadata.
-        :artist: Artist object, provides high-level interface to drawing funcitons.
-        """
-        super().draw_on_frame(frame_meta, artist)
-        for obj_meta in frame_meta.objects:
-            attr_meta = obj_meta.get_attr_meta("Secondary_CarColor", 'car_color')
-            if attr_meta is not None:
-                artist.add_text(
-                    str(attr_meta.value),
-                    int(obj_meta.bbox.left),
-                    int(obj_meta.bbox.top) + 20,
-                    anchor_point=Position.LEFT_TOP,
-                )
-            attr_meta = obj_meta.get_attr_meta("Secondary_CarMake", 'car_make')
-            if attr_meta is not None:
-                artist.add_text(
-                    str(attr_meta.value),
-                    int(obj_meta.bbox.left),
-                    int(obj_meta.bbox.top) + 38,
-                    anchor_point=Position.LEFT_TOP,
-                )
-            attr_meta = obj_meta.get_attr_meta("Secondary_VehicleTypes", 'car_type')
-            if attr_meta is not None:
-                artist.add_text(
-                    str(attr_meta.value),
-                    int(obj_meta.bbox.left),
-                    int(obj_meta.bbox.top) + 56,
-                    anchor_point=Position.LEFT_TOP,
-                )
+    def override_draw_spec(self, object_meta: ObjectMeta, draw_spec_cfg: dict) -> dict:
+        # add classifier attributes labels to the object visualisation
+        # one attribute per line
+        attr_meta = object_meta.get_attr_meta("Secondary_CarColor", 'car_color')
+        if attr_meta is not None:
+            draw_spec_cfg['label']['format'] += [str(attr_meta.value)]
+
+        attr_meta = object_meta.get_attr_meta("Secondary_CarMake", 'car_make')
+        if attr_meta is not None:
+            draw_spec_cfg['label']['format'] += [str(attr_meta.value)]
+
+        attr_meta = object_meta.get_attr_meta("Secondary_VehicleTypes", 'car_type')
+        if attr_meta is not None:
+            draw_spec_cfg['label']['format'] += [str(attr_meta.value)]
+
+        # don't forget to return the modified draw spec
+        return draw_spec_cfg
