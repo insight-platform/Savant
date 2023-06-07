64d1b97c40b3864dc8a5e4c7f265ae38d2bf63bf
Nikolai N. Bogoslovskiy <bogoslovskiy_nn@bw-sw.com>
2023-06-07 10:07:33 +0700

    updated predefined preprocessing method

--- savant/input_preproc/crop.py
+++ savant/input_preproc/crop.py
@@ -1,15 +1,18 @@
 """Model input metadata preprocessing: cropping variations."""
-import pyds
 from savant.base.input_preproc import BasePreprocessObjectMeta
+from savant.meta.bbox import BBox
+from savant.meta.object import ObjectMeta
 
 
 #  TODO Implement crop
 class CropTopPreprocessObjectMeta(BasePreprocessObjectMeta):
     """Make bbox height no greater that bbox width."""
 
-    def __call__(self, bbox: pyds.NvBbox_Coords) -> pyds.NvBbox_Coords:
+    def __call__(self, object_meta: ObjectMeta) -> BBox:
+        bbox = object_meta.bbox
         bbox_height = bbox.width
         if bbox_height > bbox.height:
             bbox_height = bbox.height
+        bbox.y_center = bbox.top + bbox_height / 2
         bbox.height = bbox_height
         return bbox
