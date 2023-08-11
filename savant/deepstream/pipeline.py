af2afd71e196e6bbe25fdd7b4fda86999638f74f
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2023-08-11 13:54:12 +0700

    #322 don't add queue just after muxer and before demuxer

--- savant/deepstream/pipeline.py
+++ savant/deepstream/pipeline.py
@@ -144,12 +144,18 @@ class NvDsPipeline(GstPipeline):
             'max-size-bytes': buffer_queues.byte_size,
             'max-size-time': 0,
         }
-        self._add_queues_to_element_group(
+        last_is_queue, remove_last_element = self._add_queues_to_element_group(
             element_group=pipeline_cfg,
             queue_properties=queue_properties,
             last_is_queue=False,
             next_should_be_queue=False,
+            first_element=True,
         )
+        if remove_last_element:
+            if isinstance(pipeline_cfg.elements[-1], ElementGroup):
+                pipeline_cfg.elements[-1].elements.pop()
+            else:
+                pipeline_cfg.elements.pop()
 
     def _add_queues_to_element_group(
         self,
@@ -157,41 +163,49 @@ class NvDsPipeline(GstPipeline):
         queue_properties: Dict[str, Any],
         last_is_queue: bool,
         next_should_be_queue: bool,
+        first_element: bool,
     ):
         """Add queues to the pipeline or an element group before and after pyfunc elements."""
 
         elements = []
+        remove_last_element = False
         for i, element in enumerate(element_group.elements):
             if isinstance(element, ElementGroup):
                 if not element.init_condition.is_enabled:
                     continue
 
-                last_is_queue = self._add_queues_to_element_group(
-                    element,
-                    queue_properties,
-                    last_is_queue,
-                    next_should_be_queue,
+                last_is_queue, remove_last_element = self._add_queues_to_element_group(
+                    element_group=element,
+                    queue_properties=queue_properties,
+                    last_is_queue=last_is_queue,
+                    next_should_be_queue=next_should_be_queue,
+                    first_element=first_element,
                 )
                 next_should_be_queue = False
+                first_element = False
                 elements.append(element)
                 continue
 
-            if (next_should_be_queue and element.element != 'queue') or (
-                isinstance(element, PyFuncElement) and not last_is_queue
-            ):
+            if (
+                (next_should_be_queue and element.element != 'queue')
+                or (isinstance(element, PyFuncElement) and not last_is_queue)
+            ) and not first_element:
                 elements.append(PipelineElement('queue', properties=queue_properties))
 
             elements.append(element)
             last_is_queue = element.element == 'queue'
             next_should_be_queue = isinstance(element, PyFuncElement)
+            first_element = False
+            remove_last_element = False
 
         if next_should_be_queue:
             elements.append(PipelineElement('queue', properties=queue_properties))
             last_is_queue = True
+            remove_last_element = True
 
         element_group.elements = elements
 
-        return last_is_queue
+        return last_is_queue, remove_last_element
 
     def _build_buffer_processor(
         self,
