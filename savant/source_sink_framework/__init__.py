f83e449fa40e500cff32d60db1b92f9200fbb6bb
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2023-08-24 13:01:00 +0700

    #342 remove combined source-sink since sink can receive more messages than source

--- savant/source_sink_framework/__init__.py
+++ savant/source_sink_framework/__init__.py
@@ -1,5 +1,4 @@
 from .builder.sink import SinkBuilder
 from .builder.source import SourceBuilder
-from .builder.source_sink import SourceSinkBuilder
 from .frame_source.jpeg import JpegSource
 from .log_provider.jaeger import JaegerLogProvider
