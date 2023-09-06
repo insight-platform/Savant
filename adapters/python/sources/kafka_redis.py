d92ab7cd41972a58e56a3c9045f686a31610d0be
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2023-09-06 14:19:07 +0700

    #400 turn KafkaRedis source to async

--- adapters/python/sources/kafka_redis.py
+++ adapters/python/sources/kafka_redis.py
@@ -1,12 +1,12 @@
+import asyncio
 import os
 import signal
+from asyncio import Event, Queue
 from distutils.util import strtobool
-from queue import Queue
-from threading import Thread
 from typing import Dict, Iterator, Optional, Tuple, Union
 
 from confluent_kafka import Consumer
-from redis import Redis
+from redis.asyncio import Redis
 from savant_rs.primitives import EndOfStream, VideoFrame, VideoFrameContent
 from savant_rs.utils.serialization import Message, load_message_from_bytes
 
@@ -16,6 +16,8 @@ from savant.utils.logging import get_logger, init_logging
 
 LOGGER_NAME = 'adapters.kafka_redis_source'
 logger = get_logger(LOGGER_NAME)
+
+STOP = object()
 
 
 def opt_config(name, default=None, convert=None):
@@ -51,34 +53,39 @@ class KafkaConfig:
 class KafkaRedisSource:
     def __init__(self, config: Config):
         self._config = config
-        self._source = SourceBuilder().with_socket(config.zmq_endpoint).build()
+        self._source = SourceBuilder().with_socket(config.zmq_endpoint).build_async()
         self._consumer = build_consumer(config.kafka)
         self._frame_clients: Dict[str, Redis] = {}
-        self._consumer_queue = Queue(config.queue_size)
         self._is_running = False
+        self._consumer_queue: Optional[Queue] = None
+        self._stop_source_event: Optional[Event] = None
 
-    def run(self):
+    async def run(self):
         logger.info('Starting the source')
-        self._is_running = True
+        self._consumer_queue = Queue(self._config.queue_size)
+        self._stop_source_event = Event()
         self._consumer.subscribe([self._config.kafka.topic])
+        self._is_running = True
         logger.info('Subscribed to topic %r', self._config.kafka.topic)
-        poller_thread = Thread(target=self.poller, name='poller')
-        poller_thread.start()
-        sender_thread = Thread(target=self.sender, name='sender')
-        sender_thread.start()
-        try:
-            sender_thread.join()
-        except KeyboardInterrupt:
-            pass
+        await asyncio.gather(self.poller(), self.sender())
+        self._stop_source_event.set()
+
+    async def stop(self):
         logger.info('Stopping the source')
         self._is_running = False
-        poller_thread.join()
-        sender_thread.join()
+        await self._stop_source_event.wait()
+        logger.info('Source was stopped')
 
-    def poller(self):
+    async def poller(self):
         logger.info('Starting poller')
+        loop = asyncio.get_running_loop()
         while self._is_running:
-            message = self._consumer.poll(self._config.kafka.poll_timeout)
+            logger.debug('Polling next message')
+            message = await loop.run_in_executor(
+                None,
+                self._consumer.poll,
+                self._config.kafka.poll_timeout,
+            )
             if message is None:
                 continue
 
@@ -91,41 +98,48 @@ class KafkaRedisSource:
                 message.key(),
                 len(data),
             )
-            self._consumer_queue.put(data)
+            await self._consumer_queue.put(data)
+        await self._consumer_queue.put(STOP)
+        logger.info('Poller was stopped')
 
-    def sender(self):
+    async def sender(self):
         logger.info('Starting sender')
-        for result in self._source.send_iter(self.video_frame_iterator()):
+        async for result in self._source.send_iter(self.video_frame_iterator()):
             logger.debug(
                 'Status of sending frame for source %s: %s.',
                 result.source_id,
                 result.status,
             )
+        logger.info('Sender was stopped')
 
-    def deserialize_message(
+    async def deserialize_message(
         self,
         data: bytes,
     ) -> Optional[Union[Tuple[VideoFrame, bytes], EndOfStream]]:
         message: Message = load_message_from_bytes(data)
         if message.is_video_frame():
             video_frame = message.as_video_frame()
-            return self.fetch_video_frame_content(video_frame)
+            return await self.fetch_video_frame_content(video_frame)
         if message.is_end_of_stream():
             return message.as_end_of_stream()
 
         logger.warning('Unsupported message type for message %r', message)
         return None
 
-    def video_frame_iterator(
+    async def video_frame_iterator(
         self,
     ) -> Iterator[Union[Tuple[VideoFrame, bytes], EndOfStream]]:
-        while self._is_running or not self._consumer_queue.empty():
-            data = self._consumer_queue.get()
-            deserialized = self.deserialize_message(data)
+        while True:
+            logger.debug('Waiting for the next message')
+            data = await self._consumer_queue.get()
+            if data is STOP:
+                logger.debug('Received stop signal')
+                break
+            deserialized = await self.deserialize_message(data)
             if deserialized is not None:
                 yield deserialized
 
-    def fetch_video_frame_content(
+    async def fetch_video_frame_content(
         self,
         video_frame: VideoFrame,
     ) -> Optional[Tuple[VideoFrame, bytes]]:
@@ -143,7 +157,9 @@ class KafkaRedisSource:
                     video_frame.content.get_method(),
                 )
                 return None
-            content = self.fetch_content_from_redis(video_frame.content.get_location())
+            content = await self.fetch_content_from_redis(
+                video_frame.content.get_location()
+            )
             if content is None:
                 logger.warning(
                     'Failed to fetch frame from %r',
@@ -156,7 +172,7 @@ class KafkaRedisSource:
         logger.warning('Unsupported frame content %r', video_frame.content)
         return None
 
-    def fetch_content_from_redis(self, location: str) -> Optional[bytes]:
+    async def fetch_content_from_redis(self, location: str) -> Optional[bytes]:
         logger.debug('Fetching frame from %r', location)
         host_port, key = location.split('/', 1)
         frame_client = self._frame_clients.get(host_port)
@@ -167,7 +183,7 @@ class KafkaRedisSource:
             frame_client = Redis(host=host, port=int(port))
             self._frame_clients[host_port] = frame_client
 
-        return frame_client.get(key)
+        return await frame_client.get(key)
 
 
 def build_consumer(config: KafkaConfig) -> Consumer:
@@ -188,7 +204,13 @@ def main():
     # To gracefully shutdown the adapter on SIGTERM (raise KeyboardInterrupt)
     signal.signal(signal.SIGTERM, signal.getsignal(signal.SIGINT))
     source = KafkaRedisSource(Config())
-    source.run()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(source.run())
+    except KeyboardInterrupt as e:
+        loop.run_until_complete(source.stop())
+    finally:
+        loop.close()
 
 
 if __name__ == '__main__':
