0b0dd083735ae16d9e3df954a12a7643470488c5
Denis Medyantsev <denisvmedyantsev@gmail.com>
2023-09-11 19:17:07 -0300

    Update bg_remover perf.

--- samples/opencv_cuda_bg_remover_mog2/run_perf.sh
+++ samples/opencv_cuda_bg_remover_mog2/run_perf.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-# you are expected to be in Savant/ directory
+# You are expected to be in Savant/ directory.
+# Usage: ./samples/opencv_cuda_bg_remover_mog2/run_perf.sh [MULTISTREAM] [MODULE_ARGS]
+# Set MULTISTREAM>0 to use multistream source with number-of-streams=MULTISTREAM,
+# the default source is `uridecodebin`.
 
-DOCKER_IMAGE=ghcr.io/insight-platform/savant-deepstream:latest
-DOCKER_RUNTIME="--gpus=all"
-if [ "$(uname -m)" = "aarch64" ]; then
-  DOCKER_IMAGE=ghcr.io/insight-platform/savant-deepstream-l4t:latest
-  DOCKER_RUNTIME="--runtime=nvidia"
+MULTISTREAM=${1:0}
+MODULE_ARGS=("$@")
+unset "MODULE_ARGS[0]"
+
+MODULE_CONFIG=samples/opencv_cuda_bg_remover_mog2/demo_performance.yml
+DATA_LOCATION=data/road_traffic.mp4
+
+if [ "$MULTISTREAM" -gt 0 ]; then
+  MODULE_ARGS+=("pipeline.source=" "parameters.shutdown_auth=shutdown" "parameters.fps_period=1000000")
+  SOURCE_ADAPTER=$(./scripts/run_source.py multi-stream --detach \
+    --number-of-streams="$MULTISTREAM" \
+    --shutdown-auth=shutdown \
+    $DATA_LOCATION)
+  trap "docker kill $SOURCE_ADAPTER >/dev/null 2>/dev/null" EXIT
+  sleep 5
 fi
 
-docker run --rm -it $DOCKER_RUNTIME \
-  -e BUFFER_QUEUES \
-  -v `pwd`/samples:/opt/savant/samples \
-  -v `pwd`/data:/data:ro \
-  $DOCKER_IMAGE \
-  samples/opencv_cuda_bg_remover_mog2/demo_performance.yml
+./scripts/run_module.py $MODULE_CONFIG "${MODULE_ARGS[@]}"
