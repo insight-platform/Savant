ecea1ff9876d6ef6f28dc10a2327a52236936166
Denis Medyantsev <denisvmedyantsev@gmail.com>
2023-09-12 09:00:41 -0300

    Update car_classification perf.

--- samples/opencv_cuda_bg_remover_mog2/run_perf.sh
+++ samples/opencv_cuda_bg_remover_mog2/run_perf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# You are expected to be in Savant/ directory.
-# Usage: ./samples/opencv_cuda_bg_remover_mog2/run_perf.sh [MULTISTREAM] [MODULE_ARGS]
-# Set MULTISTREAM>0 to use multistream source with number-of-streams=MULTISTREAM,
-# the default source is `uridecodebin`.
+# you are expected to be in Savant/ directory
 
 MULTISTREAM=${1:0}
 MODULE_ARGS=("$@")
@@ -12,7 +9,12 @@ MODULE_CONFIG=samples/opencv_cuda_bg_remover_mog2/demo_performance.yml
 DATA_LOCATION=data/road_traffic.mp4
 
 if [ "$MULTISTREAM" -gt 0 ]; then
-  MODULE_ARGS+=("pipeline.source=" "parameters.shutdown_auth=shutdown" "parameters.fps_period=1000000")
+  MODULE_ARGS+=(
+    "pipeline.source=null"
+    "parameters.shutdown_auth=shutdown"
+    "parameters.fps_period=1000000"
+    "parameters.batched_push_timeout=200000"
+  )
   SOURCE_ADAPTER=$(./scripts/run_source.py multi-stream --detach \
     --number-of-streams="$MULTISTREAM" \
     --shutdown-auth=shutdown \
