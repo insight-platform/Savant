40b1d2eb94cb16573175d3d9972a10597e2578e8
Nikolai N. Bogoslovskiy <bogoslovskiy_nn@bw-sw.com>
2023-09-21 13:17:27 +0700

    lpr demo

---
+++ samples/license_plate_recognition/overlay.py
@@ -0,0 +1,44 @@
+"""Draw func adds car classification models outputs: car color, car make, car type."""
+from savant_rs.draw_spec import LabelDraw, ObjectDraw
+
+from savant.deepstream.drawfunc import NvDsDrawFunc
+from savant.meta.object import ObjectMeta
+
+
+class Overlay(NvDsDrawFunc):
+    def override_draw_spec(
+        self, object_meta: ObjectMeta, draw_spec: ObjectDraw
+    ) -> ObjectDraw:
+        """Override draw spec for objects with label 'Car'.
+        Add classifier attributes labels to the object visualisation.
+        """
+        if object_meta.label == 'lpd':
+            new_label_format = draw_spec.label.format
+
+            # one attribute per line
+            # if there's no specific attribute for the object on this frame
+            # reserve a line for it anyway
+            # so that the object's labels don't jump up and down
+            attr_meta = object_meta.get_attr_meta('LPRNet', 'lpr')
+            if attr_meta is not None:
+                new_label_format = [str(attr_meta.value)]
+            else:
+                new_label_format = ['']
+
+            draw_spec = ObjectDraw(
+                bounding_box=draw_spec.bounding_box,
+                label=LabelDraw(
+                    font_color=draw_spec.label.font_color,
+                    border_color=draw_spec.label.border_color,
+                    background_color=draw_spec.label.background_color,
+                    padding=draw_spec.label.padding,
+                    font_scale=draw_spec.label.font_scale,
+                    thickness=draw_spec.label.thickness,
+                    format=new_label_format,
+                    position=draw_spec.label.position,
+                ),
+                central_dot=draw_spec.central_dot,
+                blur=draw_spec.blur,
+            )
+
+        return draw_spec
