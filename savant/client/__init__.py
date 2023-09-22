02d35cc26cd4fc0f490236980729de719b329f8f
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2023-09-22 15:07:16 +0700

    #400 fix importing JpegSource

--- savant/client/__init__.py
+++ savant/client/__init__.py
@@ -6,9 +6,20 @@ from .frame_source import FrameSource
 from .log_provider import LogProvider
 from .log_provider.jaeger import JaegerLogProvider
 
-try:
-    from .jpeg_source import JpegSource
-except ImportError:
-    import logging
+__all__ = [
+    'SinkBuilder',
+    'SourceBuilder',
+    'FrameSource',
+    'LogProvider',
+    'JaegerLogProvider',
+]
 
-    logging.warning('Failed to import JpegSource. Install OpenCV to enable it.')
+try:
+    from .frame_source.jpeg import JpegSource
+
+    __all__.append('JpegSource')
+except ImportError:
+    from savant.utils.logging import get_logger
+
+    logger = get_logger(__name__)
+    logger.warning('Failed to import JpegSource. Install OpenCV to enable it.')
