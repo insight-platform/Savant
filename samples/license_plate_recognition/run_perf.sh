d3f1e7007dde997daeb1c3babe5a9009261f5e79
Denis Medyantsev <denisvmedyantsev@gmail.com>
2023-10-10 06:49:14 -0300

    Fix lpr demo.

--- samples/license_plate_recognition/run_perf.sh
+++ samples/license_plate_recognition/run_perf.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # you are expected to be in Savant/ directory
 
+MULTISTREAM=${1:0}
+MODULE_ARGS=("$@")
+unset "MODULE_ARGS[0]"
+
+MODULE_CONFIG=samples/license_plate_recognition/module_performance.yml
+DATA_LOCATION=data/lpr_test_1080p.mp4
+
 if [ "$(uname -m)" = "aarch64" ]; then
-  DOCKER_RUNTIME="--runtime=nvidia"
   docker compose -f samples/license_plate_recognition/docker-compose.l4t.yml build module
 else
-  DOCKER_RUNTIME="--gpus=all"
   docker compose -f samples/license_plate_recognition/docker-compose.x86.yml build module
 fi
 
-docker run --rm -it $DOCKER_RUNTIME \
-  -e BUFFER_QUEUES \
-  -v `pwd`/samples:/opt/savant/samples \
-  -v `pwd`/data:/data:ro \
-  -v `pwd`/models/license_plate_recognition:/models \
-  -v `pwd`/downloads/license_plate_recognition:/downloads \
-  license_plate_recognition-module  \
-  samples/license_plate_recognition/module_performance.yml
+if [ "$MULTISTREAM" -gt 0 ]; then
+  MODULE_ARGS+=("pipeline.source=null" "parameters.shutdown_auth=shutdown")
+  SOURCE_ADAPTER=$(./scripts/run_source.py multi-stream --detach \
+    --number-of-streams="$MULTISTREAM" \
+    --shutdown-auth=shutdown \
+    $DATA_LOCATION)
+  trap "docker kill $SOURCE_ADAPTER >/dev/null 2>/dev/null" EXIT
+  sleep 5
+fi
+
+./scripts/run_module.py -i license_plate_recognition-module $MODULE_CONFIG "${MODULE_ARGS[@]}"
