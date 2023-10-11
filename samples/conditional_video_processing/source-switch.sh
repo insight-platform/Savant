28819e9c680159db79b7ef5e065bf26954c8e883
Denis Medyantsev <denisvmedyantsev@gmail.com>
2023-10-11 08:44:07 -0300

    Wait for etcd.

--- samples/conditional_video_processing/source-switch.sh
+++ samples/conditional_video_processing/source-switch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# Usage: source-switch.sh [on/off] [source-id]
 
 state=${1:-off}
 source=${2:-city-traffic}
 
-echo "savant/sources/$source $state"
-#docker exec -it etcd etcdctl put savant/sources/"$source" "$state"
+docker exec -it etcd etcdctl put savant/sources/"$source" "$state"
