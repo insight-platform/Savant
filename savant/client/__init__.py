c7138f84c669933c77a8bfb048b39911cac5ae82
Oleg V. Abramov <abramov.o.v@gmail.com>
2023-10-20 15:47:08 +0700

    Add PNG source

--- savant/client/__init__.py
+++ savant/client/__init__.py
@@ -2,24 +2,16 @@
 
 from .builder.sink import SinkBuilder
 from .builder.source import SourceBuilder
-from .frame_source import FrameSource
+from .frame_source import FrameSource, JpegSource, PngSource
 from .log_provider import LogProvider
 from .log_provider.jaeger import JaegerLogProvider
 
 __all__ = [
     'SinkBuilder',
     'SourceBuilder',
-    'FrameSource',
+    'JpegSource',
+    'PngSource',
     'LogProvider',
+    'FrameSource',
     'JaegerLogProvider',
 ]
-
-try:
-    from .frame_source.jpeg import JpegSource
-
-    __all__.append('JpegSource')
-except ImportError:
-    from savant.utils.logging import get_logger
-
-    logger = get_logger(__name__)
-    logger.warning('Failed to import JpegSource. Install OpenCV to enable it.')
---
+++ savant/client/utils/img_header_parse.py
@@ -0,0 +1,37 @@
+"""Image header parse utility."""
+import re
+from os import PathLike
+from typing import BinaryIO, Tuple, Union
+
+import magic
+
+from savant.gstreamer.codecs import Codec
+
+# positive lookbehind is included to avoid matching density in the JPEG header
+PATTERN = re.compile(r'(?<=, )(?P<width>\d+)( x |x)(?P<height>\d+)')
+
+
+def get_image_size_codec(file: Union[str, PathLike, BinaryIO]) -> Tuple[int, int, str]:
+    """Get JPEG or PNG image width and height by parsing the file header.
+    :param file: Path to an image file or a file handle to an image file opened as binary.
+    :return: Image width and height.
+    """
+    if hasattr(file, 'read') and hasattr(file, 'seek'):
+        magic_out = magic.from_buffer(file.read(2048))
+        file.seek(0)
+    elif isinstance(file, (str, PathLike)):
+        magic_out = magic.from_file(file)
+    else:
+        raise ValueError('File path or file handle is expected.')
+
+    if magic_out.startswith('JPEG image data'):
+        codec = Codec.JPEG.value.name
+    elif magic_out.startswith('PNG image data'):
+        codec = Codec.PNG.value.name
+    else:
+        raise ValueError('Not a JPEG or PNG file.')
+
+    match = PATTERN.search(magic_out)
+    if match:
+        return int(match['width']), int(match['height']), codec
+    raise ValueError('Failed to get image size from image header.')
