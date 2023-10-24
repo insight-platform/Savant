dfbb8f4ced02f6c853b30a87014bccab57bb5f2c
Oleg V. Abramov <abramov.o.v@gmail.com>
2023-10-24 17:28:05 +0700

    Add dependency to adapters-py

--- savant/client/__init__.py
+++ savant/client/__init__.py
@@ -2,6 +2,7 @@
 from .builder.sink import SinkBuilder
 from .builder.source import SourceBuilder
 from .frame_source import FrameSource
+from .image_source import JpegSource, PngSource
 from .log_provider import LogProvider
 from .log_provider.jaeger import JaegerLogProvider
 
@@ -11,17 +12,6 @@ __all__ = [
     'LogProvider',
     'FrameSource',
     'JaegerLogProvider',
+    'JpegSource',
+    'PngSource',
 ]
-
-try:
-    from .image_source import JpegSource, PngSource
-
-    __all__ += ['JpegSource', 'PngSource']
-except ModuleNotFoundError:
-    from savant.utils.logging import get_logger
-
-    logger = get_logger(__name__)
-    logger.warning(
-        'Image sources "python-magic" dependency is missing. '
-        'JpegSource and PngSource are not available.'
-    )
