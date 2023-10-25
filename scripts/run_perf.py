1cfd3c47701d65ce1940e68cb785c793ba8a7add
Denis Medyantsev <denisvmedyantsev@gmail.com>
2023-10-25 18:52:50 -0300

    Refactor run_perf.py.

--- scripts/run_perf.py
+++ scripts/run_perf.py
@@ -24,7 +24,7 @@ import subprocess
 import sys
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Dict, Generator, Optional, Union
+from typing import Any, Dict, List
 
 sys.path.append(str(Path(__file__).parent.parent))
 from savant.utils.platform import get_jetson_stats, get_platform_info
@@ -34,33 +34,70 @@ from savant.utils.version import version
 # They come from savant_rs logging.
 # ANSI codes interfere with parsing FPS from the output.
 ANSI_ESCAPE_PATTERN = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+FPS_PATTERN = re.compile(r'^.*Processed \d+ frames, (?P<fps>\d+\.\d+) FPS\.$')
+STATS_PATTERN = re.compile(
+    r'^(?P<name>\w+): min=(?P<min>\d+), max=(?P<max>\d+), avg=(?P<avg>\d+\.\d+)$'
+)
+
+# short measurement period to drop the 1st and the last measurements (outliers)
+# TODO: Implement delayed start and early stop of fps measurements in pipeline
+FPS_PERIOD = 100
 
 
-def launch_script(
-    args: Union[str, list], env: Optional[Dict[str, str]] = None
-) -> Generator[str, None, None]:
-    """Runs the script.
+def process_logs_file(logs_file_path: Path, stats_vars: List[str]) -> Dict[str, Any]:
+    """Get stats from log file."""
+    stats = {k: {} for k in stats_vars}
+    fps_list = []
+    with open(logs_file_path, mode='r') as log_file:
+        for line in log_file:
+            line = line.rstrip()
+            if not line:
+                continue
 
-    :param args: A string, or a sequence of script arguments.
-    :param env: The environment variables for the new process.
-    :return: Output rows
-    """
-    with subprocess.Popen(
-        args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        env=env,
-        encoding='utf8',
-        text=True,
-    ) as process:
-        while True:
-            ret_code = process.poll()
-            line = process.stdout.readline().strip()
-            yield line
-            if process.stderr:
-                yield process.stderr.readline().strip()
-            if ret_code is not None:
-                break
+            # checks won't work if there are ANSI codes in the line
+            line = ANSI_ESCAPE_PATTERN.sub('', line)
+
+            if ' Processed ' in line:
+                match = FPS_PATTERN.match(line)
+                if match:
+                    fps_list.append(float(match['fps']))
+                continue
+
+            for stats_var in stats_vars:
+                if stats_var in line:
+                    match = STATS_PATTERN.match(line)
+                    if match:
+                        stats[match['name']]['min'] = int(match['min'])
+                        stats[match['name']]['max'] = int(match['max'])
+                        stats[match['name']]['avg'] = float(match['avg'])
+                    break
+
+    # remove perhaps outliers
+    if len(fps_list) > 2:
+        fps_list = fps_list[1:-1]
+    # average
+    if len(fps_list) > 1:
+        num_frames = len(fps_list) * FPS_PERIOD
+        duration = sum({FPS_PERIOD / fps for fps in fps_list})
+        fps_list = [num_frames / duration]
+    if fps_list:
+        stats['fps'] = {'avg': fps_list[0]}
+
+    return stats
+
+
+def convert_run_cmd_to_filename(run_cmd: List[str]) -> str:
+    """Converts run_cmd args to valid file name."""
+    # use sample dir name as sample name,
+    # e.g. "samples/opencv_cuda_bg_remover_mog2/run_perf.sh"
+    # gives "opencv_cuda_bg_remover_mog2"
+    sample_name = run_cmd[0].split('/')[1]
+    # concat with other args
+    filename = sample_name + '-' + ''.join(run_cmd[1:])
+    filename = filename.lower()
+    # sanitize
+    # filename = re.sub(r'[^\w\s-]', '', filename.lower())
+    return re.sub(r'[-\s]+', '-', filename).strip('-_')
 
 
 def main():
@@ -72,9 +109,6 @@ def main():
     parser.add_argument('-n', '--num-runs', type=int, default=3, help='number of runs')
     parser.add_argument(
         '-p', '--path', type=Path, default=Path('samples'), help='path to sample(s)'
-    )
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help='print perf script output'
     )
     parser.add_argument(
         '-m', '--multi-option', action='store_true', help='multi-option launch'
@@ -144,13 +178,11 @@ def main():
         run_options = [[perf_script, '0'] for perf_script in perf_scripts]
 
     # required arguments
-    fps_period = 100
     run_args = [
         # increase time to collect batch
         '.parameters.batched_push_timeout=40000',
-        # short measurement period to drop the 1st and the last measurements (outliers)
-        # TODO: Implement delayed start and early stop of fps measurements in pipeline
-        f'.parameters.fps_period={fps_period}',
+        # correct fps_period
+        f'.parameters.fps_period={FPS_PERIOD}',
     ]
     if args.stats:
         run_args += [
@@ -161,10 +193,6 @@ def main():
             '}'
         ]
 
-    fps_pattern = re.compile(r'^.*Processed \d+ frames, (?P<fps>\d+\.\d+) FPS\.$')
-    stats_pattern = re.compile(
-        r'^(?P<name>\w+): min=(?P<min>\d+), max=(?P<max>\d+), avg=(?P<avg>\d+\.\d+)$'
-    )
     stats_vars = []
     if args.stats:
         stats_vars += [
@@ -194,85 +222,58 @@ def main():
 
     logs_root = Path('logs')
     logs_root.mkdir(parents=True, exist_ok=True)
-    log_file_name = f'{platform_info["nodename"]}-{dtm.strftime("%Y%m%d-%H%M%S")}'
+    run_name = f'{platform_info["nodename"]}-{dtm.strftime("%Y%m%d-%H%M%S")}'
     if args.multi_option:
-        log_file_name += '-multi'
-    log_file_path = logs_root / f'{log_file_name}.log'
-    json_file_path = logs_root / f'{log_file_name}.json'
-    with open(log_file_path, mode='w', encoding='utf-8') as log_file:
+        run_name += '-multi'
+    json_file_path = logs_root / f'{run_name}.json'
 
-        for run_cmd in run_options:
-            print(f'cmd: {run_cmd}')
-            log_file.write(f'cmd: {run_cmd}\n\n')
-
-            fps_list = []
-            stats = {k: {'min': [], 'max': [], 'avg': []} for k in stats_vars}
-            for num in range(args.num_runs):
+    logs_dir = logs_root / run_name
+    logs_dir.mkdir(parents=True, exist_ok=True)
+    for run_cmd in run_options:
+        logs_file_name = convert_run_cmd_to_filename(run_cmd)
+        print(f'cmd: {run_cmd}')
+        stats = []
+        for num in range(args.num_runs):
+            logs_file_path = logs_dir / f'{logs_file_name}-{num:02d}.log'
+            print(f'run #{num}')
+            # save logs
+            with open(logs_file_path, 'w') as log_file:
+                log_file.write(f'cmd: {run_cmd}\n')
                 log_file.write(f'run #{num}\n')
-
-                if is_jetson:
-                    log_file.write(f'stats: {get_jetson_stats()}\n')
-
-                _fps_list = []
-                for line in launch_script(run_cmd + run_args):
-                    if not line:
-                        continue
-
-                    log_file.write(f'{line}\n')
-                    if args.verbose:
-                        print(line, '\r')
-
-                    # checks won't work if there are ANSI codes in the line
-                    line = ANSI_ESCAPE_PATTERN.sub('', line)
-                    if ' Processed ' in line:
-                        match = fps_pattern.match(line)
-                        if match:
-                            _fps_list.append(float(match['fps']))
-                    for stats_var in stats_vars:
-                        if stats_var in line:
-                            match = stats_pattern.match(line)
-                            if match:
-                                stats[match['name']]['min'].append(int(match['min']))
-                                stats[match['name']]['max'].append(int(match['max']))
-                                stats[match['name']]['avg'].append(
-                                    round(float(match['avg']), 2)
-                                )
-                            break
-
-                if len(_fps_list) > 2:
-                    _fps_list = _fps_list[1:-1]
-                    num_frames = len(_fps_list) * fps_period
-                    duration = sum({fps_period / fps for fps in _fps_list})
-                    _fps_list = [num_frames / duration]
-                fps_list.extend(_fps_list)
-
-                log_file.write('\n')
+                # if is_jetson:
+                #     log_file.write(f'stats: {get_jetson_stats()}\n')
                 log_file.flush()
-
-            if not fps_list:
-                print('fail\n')
+                subprocess.run(run_cmd + run_args, stdout=log_file, stderr=log_file)
+            # process logs
+            _stats = process_logs_file(logs_file_path, stats_vars)
+            if 'fps' not in _stats:
+                print('fail')
                 continue
+            stats.append(_stats)
 
-            fps = sum(fps_list) / len(fps_list)
-            fps_list = [round(fps, 2) for fps in fps_list]
-            print(f'fps: {fps:.2f}\nfps_avg: {fps_list}')
+        # results
+        fps_list = [stat['fps']['avg'] for stat in stats]
+        fps = sum(fps_list) / len(fps_list)
+        fps_list = [round(fps, 2) for fps in fps_list]
+        print(f'fps: {fps:.2f}\nfps_avg: {fps_list}')
 
-            measurement = dict(
-                cmd=run_cmd,
-                fps_avg=fps_list,
-                fps=round(fps, 2),
-            )
+        measurement = dict(
+            cmd=run_cmd,
+            fps_avg=fps_list,
+            fps=round(fps, 2),
+        )
 
-            for stats_var in stats_vars:
-                for val in {'min', 'max', 'avg'}:
-                    print(f'{stats_var}_{val}: {stats[stats_var][val]}')
-                    measurement[f'{stats_var}_{val}'] = stats[stats_var][val]
+        for stats_var in stats_vars:
+            for val in ('min', 'max', 'avg'):
+                val_list = [stat[stats_var][val] for stat in stats]
+                print(f'{stats_var}_{val}: {val_list}')
+                measurement[f'{stats_var}_{val}'] = val_list
 
-            data['measurements'].append(measurement)
-            with open(json_file_path, mode='w', encoding='utf-8') as json_file:
-                json.dump(data, json_file, indent=2)
+        data['measurements'].append(measurement)
+        with open(json_file_path, mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=2)
 
-            print('\n')
+        print()
 
 
 if __name__ == '__main__':
