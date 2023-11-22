df72d7d63544a2242ec8ed8f4a00f014131079ba
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2023-11-22 14:05:16 +0700

    #556 collect metrics with timestamps

--- savant/metrics/prometheus.py
+++ savant/metrics/prometheus.py
@@ -1,6 +1,8 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
-from prometheus_client import Counter, Gauge, start_http_server
+from prometheus_client import start_http_server
+from prometheus_client.metrics_core import CounterMetricFamily, GaugeMetricFamily
+from prometheus_client.registry import Collector, REGISTRY
 from savant_rs.pipeline2 import (
     FrameProcessingStatRecord,
     FrameProcessingStatRecordType,
@@ -14,40 +16,8 @@ class PrometheusMetricsExporter(BaseMetricsExporter):
     def __init__(self, pipeline: VideoPipeline, params: Dict[str, Any]):
         super().__init__(pipeline, params)
         self._port = params['port']
-        label_names = ['record_type']
-        stage_label_names = ['record_type', 'stage_name']
-
-        self._frame_counter = Counter(
-            'frame_counter',
-            'Number of frames passed through the module',
-            labelnames=label_names,
-        )
-        self._object_counter = Counter(
-            'object_counter',
-            'Number of objects passed through the module',
-            labelnames=label_names,
-        )
-
-        self._stage_queue_length = Gauge(
-            'stage_queue_length',
-            'Queue length in the stage',
-            labelnames=stage_label_names,
-        )
-        self._stage_frame_counter = Counter(
-            'stage_frame_counter',
-            'Number of frames passed through the stage',
-            labelnames=stage_label_names,
-        )
-        self._stage_object_counter = Counter(
-            'stage_object_counter',
-            'Number of objects passed through the stage',
-            labelnames=stage_label_names,
-        )
-        self._stage_batch_counter = Counter(
-            'stage_batch_counter',
-            'Number of frame batches passed through the stage',
-            labelnames=stage_label_names,
-        )
+        self._metrics_collector = ModuleMetricsCollector()
+        REGISTRY.register(self._metrics_collector)
 
     def start(self):
         start_http_server(self._port)
@@ -75,31 +45,89 @@ class PrometheusMetricsExporter(BaseMetricsExporter):
         self._logger.debug(
             'Exporting record %s (type: %s)', record.id, record.record_type
         )
+        self._metrics_collector.set_metrics(record)
+
+
+class ModuleMetricsCollector(Collector):
+    def __init__(self):
+        self._frame_counter: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+        self._object_counter: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+        self._stage_queue_length: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+        self._stage_frame_counter: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+        self._stage_object_counter: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+        self._stage_batch_counter: Dict[Tuple[str, ...], Tuple[int, float]] = {}
+
+    def set_metrics(self, record: FrameProcessingStatRecord):
         record_type_str = _record_type_to_string(record.record_type)
-        _update_counter(
-            self._frame_counter.labels(record_type_str),
-            record.frame_no,
-        )
-        _update_counter(
-            self._object_counter.labels(record_type_str),
-            record.object_counter,
-        )
+        ts = record.ts / 1000
+        labels = (record_type_str,)
+        self._frame_counter[labels] = record.frame_no, ts
+        self._object_counter[labels] = record.object_counter, ts
         for stage in record.stage_stats:
-            self._stage_queue_length.labels(record_type_str, stage.stage_name).set(
-                stage.queue_length
-            )
-            _update_counter(
-                self._stage_frame_counter.labels(record_type_str, stage.stage_name),
-                stage.frame_counter,
-            )
-            _update_counter(
-                self._stage_object_counter.labels(record_type_str, stage.stage_name),
-                stage.object_counter,
-            )
-            _update_counter(
-                self._stage_batch_counter.labels(record_type_str, stage.stage_name),
-                stage.batch_counter,
-            )
+            stage_labels = record_type_str, stage.stage_name
+            self._stage_queue_length[stage_labels] = (stage.queue_length, ts)
+            self._stage_frame_counter[stage_labels] = (stage.frame_counter, ts)
+            self._stage_object_counter[stage_labels] = (stage.object_counter, ts)
+            self._stage_batch_counter[stage_labels] = (stage.batch_counter, ts)
+
+    def collect(self):
+        label_names = ['record_type']
+        stage_label_names = ['record_type', 'stage_name']
+        yield _build_metric(
+            'frame_counter',
+            'Number of frames passed through the module',
+            label_names,
+            self._frame_counter,
+            CounterMetricFamily,
+        )
+        yield _build_metric(
+            'object_counter',
+            'Number of objects passed through the module',
+            label_names,
+            self._object_counter,
+            CounterMetricFamily,
+        )
+        yield _build_metric(
+            'stage_queue_length',
+            'Queue length in the stage',
+            stage_label_names,
+            self._stage_queue_length,
+            GaugeMetricFamily,
+        )
+        yield _build_metric(
+            'stage_frame_counter',
+            'Number of frames passed through the stage',
+            stage_label_names,
+            self._stage_frame_counter,
+            CounterMetricFamily,
+        )
+        yield _build_metric(
+            'stage_object_counter',
+            'Number of objects passed through the stage',
+            stage_label_names,
+            self._stage_object_counter,
+            CounterMetricFamily,
+        )
+        yield _build_metric(
+            'stage_batch_counter',
+            'Number of frame batches passed through the stage',
+            stage_label_names,
+            self._stage_batch_counter,
+            CounterMetricFamily,
+        )
+
+
+def _build_metric(
+    name: str,
+    documentation: str,
+    labels: List[str],
+    values: Dict[Tuple[str, ...], Tuple[int, float]],
+    metric_class,
+):
+    counter = metric_class(name, documentation, labels=labels)
+    for labels, (value, ts) in values.items():
+        counter.add_metric(labels, value, timestamp=ts)
+    return counter
 
 
 def _record_type_to_string(record_type: FrameProcessingStatRecordType) -> str:
@@ -109,8 +137,3 @@ def _record_type_to_string(record_type: FrameProcessingStatRecordType) -> str:
         return 'timestamp'
     if record_type == FrameProcessingStatRecordType.Initial:
         return 'initial'
-
-
-def _update_counter(counter: Counter, value: int):
-    last_value = list(counter.collect())[0].samples[0].value
-    counter.inc(value - last_value)
