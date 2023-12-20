d0143d84ef04f04a88952e9b88bde743a451db0a
Mohammed Yasin <32206511+Y-T-G@users.noreply.github.com>
2023-12-20 16:29:21 +0800

    [Fix] AttributeError when ingress_frame_filter is not defined. (#597)

--- savant/config/module_config.py
+++ savant/config/module_config.py
@@ -55,7 +55,8 @@ def source_element_configurator(
     """Additional configuration steps for SourceElements."""
     # if dev mode is enabled in the module parameters
     # set dev mode for the ingress filter function
-    if module_config.parameters.dev_mode:
+    if (element_config.ingress_frame_filter is not None
+        and module_config.parameters.dev_mode):
         logger.debug(
             'Setting dev mode for ingress filter of SourceElement named "%s" to True.',
             element_config.name,
