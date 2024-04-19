c8916dd867f3cdbfb5eac21765e6b7bb49e3686b
Pavel Tomskikh <tomskikh@users.noreply.github.com>
2024-04-19 15:51:31 +0700

    Support metadata in the KVS source adapter (#737)

--- savant/gstreamer/event.py
+++ savant/gstreamer/event.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import Dict, Optional
 
 from savant.gstreamer import Gst
 
 SAVANT_EOS_EVENT_NAME = 'savant-eos'
 SAVANT_EOS_EVENT_SOURCE_ID_PROPERTY = 'source-id'
+
+SAVANT_FRAME_TAGS_EVENT_NAME = 'savant-frame-tags'
 
 
 def build_savant_eos_event(source_id: str):
@@ -33,3 +35,41 @@ def parse_savant_eos_event(event: Gst.Event) -> Optional[str]:
         return None
 
     return struct.get_string(SAVANT_EOS_EVENT_SOURCE_ID_PROPERTY)
+
+
+def build_savant_frame_tags_event(tags: Dict[str, str]):
+    """Build a savant-frame-tags event.
+
+    :param tags: Tags to set.
+    :returns: The savant-frame-tags event.
+    """
+
+    structure: Gst.Structure = Gst.Structure.new_empty(SAVANT_FRAME_TAGS_EVENT_NAME)
+    for name, value in tags.items():
+        structure.set_value(name, value)
+
+    return Gst.Event.new_custom(Gst.EventType.CUSTOM_DOWNSTREAM, structure)
+
+
+def parse_savant_frame_tags_event(event: Gst.Event) -> Optional[Dict[str, str]]:
+    """Parse a savant-frame-tags event.
+
+    :param event: The event to parse.
+    :returns: Tags if the event is a savant-frame-tags event, otherwise None.
+    """
+
+    if event.type != Gst.EventType.CUSTOM_DOWNSTREAM:
+        return None
+
+    struct: Gst.Structure = event.get_structure()
+    if not struct.has_name(SAVANT_FRAME_TAGS_EVENT_NAME):
+        return None
+
+    tags = {}
+    for i in range(struct.n_fields()):
+        name = struct.nth_field_name(i)
+        value = struct.get_string(name)
+        if value:
+            tags[name] = value
+
+    return tags
