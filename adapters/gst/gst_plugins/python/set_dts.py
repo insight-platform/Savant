77de1fa50d0f57c13e19aa3c09acb6f8042c1f9f
Pavel A. Tomskikh <tomskih_pa@bw-sw.com>
2024-04-27 15:30:24 +0700

    #742 set DTS only for frames that require it

--- adapters/gst/gst_plugins/python/set_dts.py
+++ adapters/gst/gst_plugins/python/set_dts.py
@@ -1,4 +1,6 @@
-from typing import List
+import heapq
+from collections import deque
+from typing import Deque, List
 
 from savant.gstreamer import GObject, Gst
 from savant.utils.logging import LoggerMixin
@@ -42,9 +44,11 @@ class SetDts(LoggerMixin, Gst.Element):
     def __init__(self):
         super().__init__()
 
-        self.buffers: List[Gst.Buffer] = []
+        self.buffers: Deque[Gst.Buffer] = deque()
+        self.pts_list: List[int] = []
         self.sink_pad: Gst.Pad = Gst.Pad.new_from_template(SINK_PAD_TEMPLATE, 'sink')
         self.src_pad: Gst.Pad = Gst.Pad.new_from_template(SRC_PAD_TEMPLATE, 'src')
+        self.enabled = False
 
         self.add_pad(self.sink_pad)
         self.add_pad(self.src_pad)
@@ -66,17 +70,27 @@ class SetDts(LoggerMixin, Gst.Element):
         self.logger.debug(
             'Received buffer PTS=%s from %s', buffer.pts, sink_pad.get_name()
         )
+        if not self.enabled:
+            return self.push_buffer(buffer)
 
-        if buffer.has_flags(Gst.BufferFlags.DELTA_UNIT):
+        if buffer.has_flags(Gst.BufferFlags.DELTA_UNIT) and (
+            self.buffers or buffer.dts == Gst.CLOCK_TIME_NONE
+        ):
             self.buffers.append(buffer)
+            heapq.heappush(self.pts_list, buffer.pts)
+            flow_ret = Gst.FlowReturn.OK
+
         else:
-            self.push_pending_buffers()
+            flow_ret = self.push_pending_buffers()
+            if flow_ret != Gst.FlowReturn.OK:
+                return flow_ret
+
             buffer.dts = buffer.pts
-            self.push_buffer(buffer)
+            flow_ret = self.push_buffer(buffer)
 
-        return Gst.FlowReturn.OK
+        return flow_ret
 
-    def on_pad_event(self, pad: Gst.Pad, info: Gst.PadProbeInfo):
+    def on_pad_event(self, pad: Gst.Pad, info: Gst.PadProbeInfo) -> Gst.PadProbeReturn:
         """Handle sink pad event."""
 
         event: Gst.Event = info.get_event()
@@ -85,26 +99,50 @@ class SetDts(LoggerMixin, Gst.Element):
             self.logger.info('Got EOS from %s', pad.get_name())
             self.push_pending_buffers()
 
-        return self.src_pad.push_event(event)
+        if event.type == Gst.EventType.CAPS:
+            caps: Gst.Caps = event.parse_caps()
+            self.logger.debug('Got CAPS %s', caps.to_string())
+            caps_name = caps[0].get_name()
+            self.enabled = caps_name.startswith('video/') and not caps_name.startswith(
+                'video/x-raw'
+            )
+            self.push_pending_buffers()
 
-    def push_pending_buffers(self):
+        elif event.type == Gst.EventType.SEGMENT:
+            self.push_pending_buffers()
+
+        elif event.type == Gst.EventType.STREAM_START:
+            self.push_pending_buffers()
+
+        self.src_pad.push_event(event)
+
+        return Gst.PadProbeReturn.OK
+
+    def push_pending_buffers(self) -> Gst.FlowReturn:
         """Push pending buffers to the src pad."""
 
         self.logger.debug('Pushing %s pending buffers', len(self.buffers))
-        if not self.buffers:
-            return
-        pending_pts = [x.pts for x in self.buffers]
-        pending_pts.sort()
-        for x, ts in zip(self.buffers, pending_pts):
-            x.dts = ts
-            self.push_buffer(x)
-        self.buffers = []
+        while self.buffers:
+            buffer = self.buffers.popleft()
+            ts = heapq.heappop(self.pts_list)
+            buffer.dts = ts
+            flow_ret = self.push_buffer(buffer)
+            if flow_ret != Gst.FlowReturn.OK:
+                return flow_ret
 
-    def push_buffer(self, buffer: Gst.Buffer):
+        return Gst.FlowReturn.OK
+
+    def push_buffer(self, buffer: Gst.Buffer) -> Gst.FlowReturn:
         """Push buffer to the src pad."""
 
         self.logger.debug('Pushing buffer PTS=%s', buffer.pts)
-        return self.src_pad.push(buffer)
+        flow_ret = self.src_pad.push(buffer)
+        if flow_ret == Gst.FlowReturn.OK:
+            self.logger.debug('Buffer PTS=%s successfully pushed', buffer.pts)
+        else:
+            self.logger.error('Failed to push buffer PTS=%s: %s', buffer.pts, flow_ret)
+
+        return flow_ret
 
 
 # register plugin
