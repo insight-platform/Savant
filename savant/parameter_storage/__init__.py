abcff7371d574c69e33c52e3dbf21a18b15cd52e
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2024-06-18 13:28:57 +0200

    implemented TLS support for Etcd

--- savant/parameter_storage/__init__.py
+++ savant/parameter_storage/__init__.py
@@ -9,12 +9,40 @@ from savant_rs.match_query import (
     register_env_resolver,
     register_etcd_resolver,
     register_utility_resolver,
+    TlsConfig,
+    EtcdCredentials,
 )
+import pathlib
 
 __all__ = ['param_storage', 'init_param_storage']
 
 ParameterStorage = Dict[str, Any]
 __PARAM_STORAGE: Optional[ParameterStorage] = None
+
+
+@dataclass
+class EtcdCredentialsConfig:
+    """Etcd credentials."""
+
+    username: str
+    """The username."""
+
+    password: str
+    """The password."""
+
+
+@dataclass
+class TlsConfigConfig:
+    """TLS configuration parameters."""
+
+    ca: pathlib.Path
+    """The certificate authority file path."""
+
+    cert: pathlib.Path
+    """The client certificate file path."""
+
+    key: pathlib.Path
+    """The client key file path."""
 
 
 @dataclass
@@ -24,8 +52,11 @@ class EtcdStorageConfig:
     hosts: List[str]
     """The list of Etcd hosts to connect to. E.g. ["127.0.0.1:2379"]."""
 
-    credentials: Optional[Tuple[str, str]] = None
+    credentials: Optional[EtcdCredentialsConfig] = None
     """The credentials to use for authentication."""
+
+    tls_config: Optional[TlsConfigConfig] = None
+    """The TLS configuration."""
 
     watch_path: str = 'savant'
     """The path in Etcd used as the source of the state."""
@@ -98,7 +129,31 @@ def init_param_storage(config: DictConfig) -> None:
 
     if 'etcd' in config.parameters and config.parameters.etcd:
         storage_config_schema = OmegaConf.structured(EtcdStorageConfig)
-        storage_config = OmegaConf.unsafe_merge(
+        storage_config: EtcdStorageConfig = OmegaConf.unsafe_merge(
             storage_config_schema, config.parameters.etcd
         )
-        register_etcd_resolver(**storage_config)
+        hosts: List[str] = storage_config.hosts
+        credentials: EtcdCredentials = None
+        if storage_config.credentials:
+            credentials = EtcdCredentials(
+                storage_config.credentials.username, storage_config.credentials.password
+            )
+        tls_config: TlsConfig = None
+        if storage_config.tls_config:
+            tls_config = TlsConfig(
+                storage_config.tls_config.ca.read_text(),
+                storage_config.tls_config.cert.read_text(),
+                storage_config.tls_config.key.read_text(),
+            )
+        watch_path: str = storage_config.watch_path
+        connect_timeout: int = storage_config.connect_timeout
+        watch_path_wait_timeout: int = storage_config.watch_path_wait_timeout
+
+        register_etcd_resolver(
+            hosts,
+            credentials,
+            tls_config,
+            watch_path,
+            connect_timeout,
+            watch_path_wait_timeout,
+        )
