6d1c5663a3ef3b6de927a7a3057259ec9ca05800
Ivan Kud <bwsw@users.noreply.github.com>
2024-06-19 14:56:22 +0200

    787 Etcd TLS support (#790)
    * module image types documentation
    
    * implemented TLS support for Etcd
    
    * implemented TLS support for Etcd resolver
    
    * implemented TLS support for Etcd resolver
    
    * updated etcd documentation
    
    * updated etcd documentation
    
    * updated etcd configuration
    
    * updated etcd configuration
    
    * updated requests (dependabot alerts)
    
    * updated requests (dependabot alerts)
    
    * Reformat.
    
    * Fix logging.
    
    * Fix numpy version.
    
    ---------
    
    Co-authored-by: Denis Medyantsev <denisvmedyantsev@gmail.com>

--- savant/parameter_storage/__init__.py
+++ savant/parameter_storage/__init__.py
@@ -1,20 +1,51 @@
 """Parameter storage package."""
 
+import pathlib
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple
 
 from omegaconf import DictConfig, ListConfig, OmegaConf
 from savant_rs.match_query import (
+    EtcdCredentials,
+    TlsConfig,
     register_config_resolver,
     register_env_resolver,
     register_etcd_resolver,
     register_utility_resolver,
 )
 
+from savant.utils.logging import get_logger
+
 __all__ = ['param_storage', 'init_param_storage']
 
 ParameterStorage = Dict[str, Any]
 __PARAM_STORAGE: Optional[ParameterStorage] = None
+logger = get_logger(__name__)
+
+
+@dataclass
+class EtcdCredentialsConfig:
+    """Etcd credentials."""
+
+    username: str
+    """The username."""
+
+    password: str
+    """The password."""
+
+
+@dataclass
+class TlsParametersConfig:
+    """TLS configuration parameters."""
+
+    ca_certificate: pathlib.Path
+    """The certificate authority file path."""
+
+    certificate: pathlib.Path
+    """The client certificate file path."""
+
+    key: pathlib.Path
+    """The client key file path."""
 
 
 @dataclass
@@ -24,8 +55,11 @@ class EtcdStorageConfig:
     hosts: List[str]
     """The list of Etcd hosts to connect to. E.g. ["127.0.0.1:2379"]."""
 
-    credentials: Optional[Tuple[str, str]] = None
+    credentials: Optional[EtcdCredentialsConfig] = None
     """The credentials to use for authentication."""
+
+    tls: Optional[TlsParametersConfig] = None
+    """The TLS configuration."""
 
     watch_path: str = 'savant'
     """The path in Etcd used as the source of the state."""
@@ -98,7 +132,44 @@ def init_param_storage(config: DictConfig) -> None:
 
     if 'etcd' in config.parameters and config.parameters.etcd:
         storage_config_schema = OmegaConf.structured(EtcdStorageConfig)
-        storage_config = OmegaConf.unsafe_merge(
+        storage_config: EtcdStorageConfig = OmegaConf.unsafe_merge(
             storage_config_schema, config.parameters.etcd
         )
-        register_etcd_resolver(**storage_config)
+        hosts: List[str] = storage_config.hosts
+        credentials: EtcdCredentials = None
+        if storage_config.credentials:
+            credentials = EtcdCredentials(
+                storage_config.credentials.username, storage_config.credentials.password
+            )
+        tls_config: TlsConfig = None
+        if storage_config.tls:
+            try:
+                logger.info(
+                    'Loading Etcd CA from %s.', storage_config.tls.ca_certificate
+                )
+                ca_certificate = storage_config.tls.ca_certificate.read_text()
+                logger.info(
+                    'Loading Etcd client cert from %s.', storage_config.tls.certificate
+                )
+                cert = storage_config.tls.certificate.read_text()
+                logger.info('Loading Etcd client key from %s.', storage_config.tls.key)
+                key = storage_config.tls.key.read_text()
+                tls_config = TlsConfig(ca_certificate, cert, key)
+            except FileNotFoundError as exc:
+                raise FileNotFoundError(
+                    f'File not found when loading Etcd CA, cert or key: {exc.filename}.'
+                    ' Please check the path to the file.'
+                )
+
+        watch_path: str = storage_config.watch_path
+        connect_timeout: int = storage_config.connect_timeout
+        watch_path_wait_timeout: int = storage_config.watch_path_wait_timeout
+
+        register_etcd_resolver(
+            hosts,
+            credentials,
+            tls_config,
+            watch_path,
+            connect_timeout,
+            watch_path_wait_timeout,
+        )
