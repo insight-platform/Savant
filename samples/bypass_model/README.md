dd8174f27848a82eb252a33863458c108f567d32
Xenia Mihaleva <mikhaljova_ka@bw-sw.com>
2024-08-07 13:44:18 +0400

    Adding a new demo with identity model (#824)
    * Adding a new demo with identity model
    * Adding instructions on how to export an onnx model
    * Stream raw image and pre-processed image to show difference
    * updated readme
    * Fixing todos
    * Updating readme with an explanation of the result
    
    ---------
    
    Co-authored-by: Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>

---
+++ samples/bypass_model/README.md
@@ -0,0 +1,117 @@
+# Bypass Model
+
+The pipeline shows the usage of the bypass model, which is a simple model that does not perform any computation on the
+input data. It demonstrates how data is pre-processed before being processed by the model and can be used for
+troubleshooting. The demo uses a `maintain_aspect_ratio` flag to show how pre-processed data can be compared with the
+raw data.
+
+Identity model is prepared by converting `nn.Identity` PyTorch model to ONNX format (using
+standard `torch.onnx.export`). For more details, please refer to the [export ONNX model](#export-onnx-model) section.
+
+Tested on platforms:
+
+- Nvidia Turing
+- Nvidia Jetson Orin family
+
+## Prerequisites
+
+```bash
+git clone https://github.com/insight-platform/Savant.git
+cd Savant
+git lfs pull
+./utils/check-environment-compatible
+```
+
+**Note**: Ubuntu 22.04 runtime
+configuration [guide](https://insight-platform.github.io/Savant/develop/getting_started/0_configure_prod_env.html) helps
+to configure the runtime to run Savant pipelines.
+
+## Build Engines
+
+The demo uses models that are compiled into TensorRT engines the first time the demo is run. This takes time.
+Optionally, you can prepare the engines before running the demo by using the command:
+
+```bash
+# you are expected to be in Savant/ directory
+
+./scripts/run_module.py --build-engines samples/bypass_model/demo.yml
+```
+
+## Run Demo
+
+```bash
+# you are expected to be in Savant/ directory
+
+# if x86
+docker compose -f samples/bypass_model/docker-compose.x86.yml up -d
+
+# if Jetson
+docker compose -f samples/bypass_model/docker-compose.l4t.yml up -d
+```
+
+## View Output
+
+A result video can be viewed:
+- in the browser at `http://127.0.0.1:888/stream/video-with-preprocessed-frame/' (LL-HLS)
+- in a player using `rtsp://127.0.0.1:554/stream/video-with-preprocessed-frame
+
+The video consists of original video stream and the pre-processed video stream side by side from left to right. The white background highlights the original video frame because its size is different from the size of the pre-processed frame.
+The pre-processed frame has a black background at the bottom, which is the result of the `maintain_aspect_ratio` flag.
+
+Example output frame:
+![](assets/result.jpg)
+
+## Clean Up
+
+```bash
+# you are expected to be in Savant/ directory
+
+# if x86
+docker compose -f samples/bypass_model/docker-compose.x86.yml down -v
+
+# if Jetson
+docker compose -f samples/bypass_model/docker-compose.l4t.yml down -v
+```
+
+## How To Obtain ONNX Identity Model From PyTorch
+
+The model takes input in the form of dynamic tensor axes `batch x 3 x height x width`, where `batch`, `height`,
+and `width` are defined at runtime.
+
+This allows providing the model with sources of different shapes. If you need to change the shape of the input tensor,
+you will have to make modifications to the [export.py](export.py) script. After these modifications, you can export the
+model using the following command:
+
+```bash
+# you are expected to be in Savant/ directory
+
+docker run --rm \
+  -v "$(pwd)/samples/bypass_model:/opt/bypass_model" \
+  -w /opt/bypass_model \
+  --user "$(id -u):$(id -g)" \
+  --entrypoint python \
+  ghcr.io/insight-platform/savant-deepstream-extra \
+  /opt/bypass_model/export.py
+
+```
+
+When the command completes, the model definition will be printed in the console output. It must look like this:
+
+```
+graph main_graph (
+  %input[FLOAT, batchx3xheightxwidth]
+) {
+  %output = Identity(%input)
+  return %output
+}
+```
+
+After running the command, the ONNX model will be saved in the `samples/bypass_model` directory. To use the model in the
+pipeline, you need to change `demo.yml` to replace `remote` section with the following:
+
+```yaml
+local_path: /cache/models/bypass_model/identity-local
+```
+
+And uncomment the volume mount in the `module` service in [docker-compose.x86.yml](docker-compose.x86.yml)
+or [docker-compose.l4t.yml](docker-compose.l4t.yml) file.
---
+++ samples/bypass_model/example.py
@@ -0,0 +1,130 @@
+"""Example of a Python plugin for comparing pre-processed and raw frames."""
+
+from typing import Dict
+
+import cupy as cp
+
+from savant.deepstream import opencv_utils
+from savant.deepstream.meta.frame import NvDsFrameMeta
+from savant.deepstream.opencv_utils import nvds_to_gpu_mat
+from savant.deepstream.pyfunc import NvDsPyFuncPlugin
+from savant.gstreamer import Gst
+from savant.parameter_storage import param_storage
+from savant.utils.memory_repr import cupy_array_as_opencv_gpu_mat
+
+ELEMENT_NAME = param_storage()['element_name']
+ATTR_NAME = param_storage()['attribute_name']
+PREPROCESSED_RESOLUTION = (
+    param_storage()['result_shape'][2],
+    param_storage()['result_shape'][1],
+)
+RESULT_RESOLUTION = (PREPROCESSED_RESOLUTION[0] * 2, PREPROCESSED_RESOLUTION[1])
+
+
+class DisplayFrames(NvDsPyFuncPlugin):
+    """Place raw and pre-processed frames in the result stream for visual comparison."""
+
+    def __init__(
+        self,
+        codec_params: Dict,
+        **kwargs,
+    ):
+        self.codec_params = codec_params
+        self.result_aux_stream = None
+
+        super().__init__(**kwargs)
+
+    def on_source_add(self, source_id: str):
+        """Initialize an auxiliary stream for result."""
+
+        self.logger.info('Source %s added.', source_id)
+
+        result_source_id = f'{source_id}-with-preprocessed-frame'
+        self.logger.info(
+            'Creating result stream %s for source %s. Resolution: %s.',
+            result_source_id,
+            source_id,
+            RESULT_RESOLUTION,
+        )
+        self.result_aux_stream = self.auxiliary_stream(
+            source_id=result_source_id,
+            width=RESULT_RESOLUTION[0],
+            height=RESULT_RESOLUTION[1],
+            codec_params=self.codec_params,
+        )
+
+    def process_frame(self, buffer: Gst.Buffer, frame_meta: NvDsFrameMeta):
+        """Process frame metadata.
+
+        :param buffer: Gstreamer buffer with this frame's data.
+        :param frame_meta: This frame's metadata.
+        """
+
+        # Get CUDA stream for asynchronous processing
+        cuda_stream = self.get_cuda_stream(frame_meta)
+        with nvds_to_gpu_mat(buffer, frame_meta.frame_meta) as frame_mat:
+            element_attr = None
+            for obj_meta in frame_meta.objects:
+                if obj_meta.is_primary:
+                    element_attr = obj_meta.get_attr_meta(ELEMENT_NAME, ATTR_NAME)
+                    break
+
+            # Transform super resolution image
+            if element_attr:
+                # CHW => HWC
+                preprocessed_image = cp.transpose(element_attr.value, (1, 2, 0))
+                # RGB => RGBA
+                preprocessed_image = cp.dstack(
+                    (
+                        preprocessed_image.astype(cp.uint8),
+                        cp.full(PREPROCESSED_RESOLUTION[::-1], 255, dtype=cp.uint8),
+                    )
+                )
+                start_point = (preprocessed_image.shape[1], 0)
+                preprocessed_image_mat = cupy_array_as_opencv_gpu_mat(
+                    preprocessed_image
+                )
+
+                # Create frame for the auxiliary stream.
+                # The frame will be sent automatically
+                aux_frame, aux_buffer = self.result_aux_stream.create_frame(
+                    pts=frame_meta.pts,
+                    duration=frame_meta.duration,
+                )
+                with nvds_to_gpu_mat(aux_buffer, batch_id=0) as aux_mat:
+                    # Create a background image
+                    white_image = cp.full(
+                        (RESULT_RESOLUTION[1], RESULT_RESOLUTION[0], 4),
+                        255,
+                        dtype=cp.uint8,
+                    )
+                    background = cupy_array_as_opencv_gpu_mat(white_image)
+                    opencv_utils.alpha_comp(
+                        aux_mat, background, (0, 0), stream=cuda_stream
+                    )
+
+                    # Place original frame and pre-processed frame side by side
+                    opencv_utils.alpha_comp(
+                        aux_mat,
+                        frame_mat,
+                        (0, 0),
+                        stream=cuda_stream,
+                    )
+                    opencv_utils.alpha_comp(
+                        aux_mat,
+                        preprocessed_image_mat,
+                        start_point,
+                        stream=cuda_stream,
+                    )
+            else:
+                self.logger.warning(
+                    'Model %s attribute %s not found.', ELEMENT_NAME, ATTR_NAME
+                )
+
+    def on_stop(self) -> bool:
+        self.result_aux_stream = None
+        return super().on_stop()
+
+    def on_source_eos(self, source_id: str):
+        self.logger.info('Got EOS from source %s.', source_id)
+        self.result_aux_stream.eos()
