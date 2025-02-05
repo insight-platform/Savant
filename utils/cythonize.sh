dc6720e167cce024277351d3001f06edfbd2d272
Denis Medyantsev <denisvmedyantsev@gmail.com>
2025-02-05 08:10:12 +0100

    Update cythonize script.

--- utils/cythonize.sh
+++ utils/cythonize.sh
@@ -7,19 +7,21 @@ set -x
 python3 -m pip install --no-cache-dir cython
 
 if [ -d "savant" ]; then
-    find savant -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
-    find savant -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
-    find savant -name "*.c" -delete
+  find savant -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
+  find savant -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
 fi
 
 if [ -d "adapters" ]; then
-    find adapters -path adapters/gst/gst_plugins/python -prune -o -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
-    find adapters -path adapters/gst/gst_plugins/python -prune -o -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
-    find adapters -name "*.c" -delete
+  exclude_dirs="adapters/gst/gst_plugins adapters/gst/sinks adapters/python/bridge adapters/python/sinks adapters/python/sources"
+  exclude_dirs=${exclude_dirs// / -o -path }
+  find adapters \( -path $exclude_dirs \) -prune -o -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
+  find adapters \( -path $exclude_dirs \) -prune -o -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
 fi
 
 if [ -d "watchdog" ]; then
-    find watchdog -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
-    find watchdog -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
-    find watchdog -name "*.c" -delete
+  find watchdog -name "*.py" -not -name "__main__.py" -print0 | xargs -0 cythonize -i -3
+  find watchdog -name "*.py" -not -name "__main__.py" -print0 | xargs -0 rm
 fi
+
+find . -name "*.c" -type f -delete
+find . -name "build" -type d -print0 | xargs -0 rm -rf
