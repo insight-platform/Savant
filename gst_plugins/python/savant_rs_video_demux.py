494c032599168e760465c834b31b8eee646fdbc8
Denis Medyantsev <denisvmedyantsev@gmail.com>
2025-03-12 10:00:23 +0100

    Change the behavior when the maximum number of streams is reached from an error to a warning and discard new streams.

--- gst_plugins/python/savant_rs_video_demux.py
+++ gst_plugins/python/savant_rs_video_demux.py
@@ -411,20 +411,12 @@ class SavantRsVideoDemux(LoggerMixin, Gst.Element):
                 self.max_parallel_streams
                 and len(self.sources) >= self.max_parallel_streams
             ):
-                self.is_running = False
-                error = (
-                    f'Failed to add source {frame_info.video_frame.source_id!r}: reached maximum '
-                    f'number of streams: {self.max_parallel_streams}.'
+                message = (
+                    f'Failed to add source {frame_info.video_frame.source_id!r}: '
+                    f'reached maximum number of streams: {self.max_parallel_streams}.'
                 )
-                self.logger.error(error)
-                frame = inspect.currentframe()
-                gst_post_stream_demux_error(
-                    gst_element=self,
-                    frame=frame,
-                    file_path=__file__,
-                    text=error,
-                )
-                return Gst.FlowReturn.ERROR
+                self.logger.warning(message)
+                return Gst.FlowReturn.OK
 
             if not frame_info.video_frame.keyframe:
                 self.logger.warning(
