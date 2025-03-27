ec7d5b5a7edb2633624fb9196947ee97b4bea334
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-03-27 21:29:23 +0100

    fixed conflict

--- savant/remote_file/__init__.py
+++ savant/remote_file/__init__.py
@@ -11,7 +11,6 @@ from .base import RemoteFileError, RemoteFileManagerType
 from .gcs import GCSFileHandler
 from .http import HTTPFileHandler
 from .s3 import S3FileHandler
-from .gcs import GCSFileHandler
 from .schema import RemoteFile
 from .utils import read_file_checksum, unpack_archive
 
