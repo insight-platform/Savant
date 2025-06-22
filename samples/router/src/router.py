13b165935667171e17b5b05c8c766483881fc2b6
Ivan Kud <bwsw@users.noreply.github.com>
2025-06-22 12:52:30 +0200

    updated router demo to show how to use savant-rs specialized image with a sample analytics implementation. (#1008)

--- samples/router/src/router.py
+++ samples/router/src/router.py
@@ -3,6 +3,13 @@ from typing import Any
 
 from savant_rs import register_handler
 from savant_rs.logging import LogLevel, log
+from savant_rs.primitives import (
+    Attribute,
+    AttributeValue,
+    IdCollisionResolutionPolicy,
+    VideoObject,
+)
+from savant_rs.primitives.geometry import BBox
 from savant_rs.utils.serialization import Message
 
 
@@ -20,14 +27,14 @@ class IngressHandler:
     ):
         log(
             LogLevel.Debug,
-            "ingress_handler",
-            f"Received message from {ingress_name} for topic {topic}",
+            'ingress_handler',
+            f'Received message from {ingress_name} for topic {topic}',
         )
         # message.labels = [branches[0]]
         log(
             LogLevel.Debug,
-            "ingress_handler",
-            f"Sending message from {ingress_name} for topic {topic} with labels {message.labels}",
+            'ingress_handler',
+            f'Sending message from {ingress_name} for topic {topic} with labels {message.labels}',
         )
 
         if message.is_video_frame():
@@ -40,9 +47,43 @@ class IngressHandler:
                 self.stream_screenshot_scheduler[topic] = 0
             if now - self.stream_screenshot_scheduler[topic] > self.period:
                 self.stream_screenshot_scheduler[topic] = now
-                # TODO: save screenshot
-                log(LogLevel.Info, "ingress_handler", f"Marking a frame as a screenshot for {topic}")
-                message.labels = ["screenshots"]
+                log(
+                    LogLevel.Info,
+                    'ingress_handler',
+                    f'Marking a frame as a screenshot for {topic}',
+                )
+                message.labels = ['screenshots']
+
+            obj = VideoObject(
+                id=1,
+                namespace='custom',
+                label='sampler',
+                detection_box=BBox.ltrb(0, 0, frame.width, frame.height).as_rbbox(),
+                confidence=1.0,
+                attributes=[],
+                track_id=None,
+                track_box=None,
+            )
+            frame.add_object(obj, IdCollisionResolutionPolicy.Error)
+            frame.set_attribute(
+                Attribute(
+                    namespace='custom',
+                    name='attribute',
+                    hint=None,
+                    values=[
+                        AttributeValue.bytes(
+                            dims=[8, 3, 8, 8],
+                            blob=bytes(3 * 8 * 8),
+                            confidence=None,
+                        ),
+                        AttributeValue.bytes_from_list(
+                            dims=[4, 1], blob=[0, 1, 2, 3], confidence=None
+                        ),
+                        AttributeValue.integer(1, confidence=0.5),
+                        AttributeValue.float(1.0, confidence=0.5),
+                    ],
+                )
+            )
 
         return message
 
@@ -51,8 +92,12 @@ def init(params: Any):
     """
     This function is called once when the service starts. It is specified in the configuration.json file.
     """
-    log(LogLevel.Info, "router::init", f"Initializing router service with params: {params}")
-    screenshot_period = params.get("screenshot_period", 10)
-    register_handler("ingress_handler", IngressHandler(screenshot_period))
-    log(LogLevel.Info, "router::init", "Router service initialized successfully")
+    log(
+        LogLevel.Info,
+        'router::init',
+        f'Initializing router service with params: {params}',
+    )
+    screenshot_period = params.get('screenshot_period', 10)
+    register_handler('ingress_handler', IngressHandler(screenshot_period))
+    log(LogLevel.Info, 'router::init', 'Router service initialized successfully')
     return True
--- samples/router/src/screenshot_maker.py
+++ samples/router/src/screenshot_maker.py
@@ -1,9 +1,10 @@
 import cv2
+from savant_rs.logging import LogLevel, log
+
 from savant.deepstream.drawfunc import NvDsDrawFunc
 from savant.deepstream.meta.frame import NvDsFrameMeta
 from savant.deepstream.opencv_utils import nvds_to_gpu_mat
 from savant.gstreamer import Gst
-from savant_rs.logging import LogLevel, log
 
 
 class Overlay(NvDsDrawFunc):
@@ -15,13 +16,13 @@ class Overlay(NvDsDrawFunc):
         with nvds_to_gpu_mat(buffer, frame_meta.frame_meta) as frame_mat:
             log(
                 LogLevel.Info,
-                "overlay",
-                f"Making a screenshot for stream {frame_meta.source_id} frame {uuid}",
+                'overlay',
+                f'Making a screenshot for stream {frame_meta.source_id} frame {uuid}',
             )
             cpu_mat = frame_mat.download()
             bgr_image = cv2.cvtColor(cpu_mat, cv2.COLOR_RGBA2BGR)
             cv2.imwrite(
-                f"/screenshots/{frame_meta.source_id}-{uuid}.jpg",
+                f'/screenshots/{frame_meta.source_id}-{uuid}.jpg',
                 bgr_image,
-                [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                [int(cv2.IMWRITE_JPEG_QUALITY), 90],
             )
