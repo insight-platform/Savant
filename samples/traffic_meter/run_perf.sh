19a313d18a43c119f95468fd67cc749fc0d10344
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-07-29 13:56:28 +0200

    removed deepstream yolo postporcessing.

--- samples/traffic_meter/run_perf.sh
+++ samples/traffic_meter/run_perf.sh
@@ -18,6 +18,6 @@ fi
 source samples/assets/run_perf_helper.sh
 set_source $DATA_LOCATION
 PERF_CONFIG="${MODULE_CONFIG%.*}_perf.yml"
-YQ_ARGS+=('.parameters.detector="yolov8m"' '.parameters.send_stats=False')
+YQ_ARGS+=('.parameters.detector="yolov8s"' '.parameters.send_stats=False')
 config_perf $MODULE_CONFIG $PERF_CONFIG "${YQ_ARGS[@]}"
 ./scripts/run_module.py -i traffic_meter-module $PERF_CONFIG
