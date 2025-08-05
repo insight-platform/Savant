e06638fa193a00a1407cef4a23734c5edcbe7b60
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-08-05 16:19:27 +0200

    Removed unused imports

--- savant/api/builder.py
+++ savant/api/builder.py
@@ -3,10 +3,8 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 from savant_rs.primitives import (
     Attribute,
     AttributeValue,
-    IdCollisionResolutionPolicy,
     VideoFrame,
     VideoFrameContent,
-    VideoObject,
 )
 from savant_rs.primitives.geometry import RBBox
 
