d812db93cf1ab3c8d47d7c4758f8d8ce519a4182
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-08-20 17:03:31 +0200

    added nvimagecodec and replaced screenshotmaker implementation in router with nvimagecodec

--- samples/router/src/screenshot_maker.py
+++ samples/router/src/screenshot_maker.py
@@ -5,24 +5,25 @@ from savant.deepstream.drawfunc import NvDsDrawFunc
 from savant.deepstream.meta.frame import NvDsFrameMeta
 from savant.deepstream.opencv_utils import nvds_to_gpu_mat
 from savant.gstreamer import Gst
+from nvidia import nvimgcodec
+from savant.utils.memory_repr import opencv_gpu_mat_as_cupy_array
 
 
 class Overlay(NvDsDrawFunc):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.codec = nvimgcodec.Encoder(device_id=0)
+        self.encode_params = nvimgcodec.EncodeParams(quality_type=nvimgcodec.QualityType.QUALITY, quality_value=95)
 
     def draw(self, buffer: Gst.Buffer, frame_meta: NvDsFrameMeta):
         uuid = frame_meta.video_frame.uuid
+        cuda_stream = self.get_cuda_stream(frame_meta)
         with nvds_to_gpu_mat(buffer, frame_meta.frame_meta) as frame_mat:
             log(
                 LogLevel.Info,
                 'overlay',
                 f'Making a screenshot for stream {frame_meta.source_id} frame {uuid}',
             )
-            cpu_mat = frame_mat.download()
-            bgr_image = cv2.cvtColor(cpu_mat, cv2.COLOR_RGBA2BGR)
-            cv2.imwrite(
-                f'/screenshots/{frame_meta.source_id}-{uuid}.jpg',
-                bgr_image,
-                [int(cv2.IMWRITE_JPEG_QUALITY), 90],
-            )
+            rgb_image = cv2.cuda.cvtColor(frame_mat, cv2.COLOR_RGBA2RGB)
+            cupy_image = opencv_gpu_mat_as_cupy_array(rgb_image) # zero copy
+            self.codec.write(f'/screenshots/{frame_meta.source_id}-{uuid}.jpg', cupy_image, "jpeg", params=self.encode_params, cuda_stream=cuda_stream.cudaPtr())
