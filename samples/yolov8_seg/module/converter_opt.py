f50ec9f9fee658c7dc2386d0398fc920aed73ae7
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-08-21 12:13:44 +0200

    optimized function

--- samples/yolov8_seg/module/converter_opt.py
+++ samples/yolov8_seg/module/converter_opt.py
@@ -12,7 +12,5 @@ def converter_selector() -> str:
 
 def output_frame_selector() -> Dict[str, Any]:
     visualize_tag = {'tag': 'visualize'}
-    if is_aarch64():
-        return {'codec': 'jpeg', 'condition': visualize_tag}
-    else:
-        return {'codec': 'hevc', 'condition': visualize_tag}
+    codec = 'jpeg' if is_aarch64() else 'hevc'
+    return {'codec': codec, 'condition': visualize_tag}
