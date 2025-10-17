2b7c13043697f8989a3da15b64fb5cadbefe9d2f
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-10-17 17:34:59 +0200

    Moved logs to savant-rs to ensure all apps can use them, not only Savant.

---
+++ savant/utils/log.py
@@ -0,0 +1,5 @@
+"""Logging utils package."""
+
+from savant_rs.py.log import LoggerMixin, get_logger, init_logging, update_logging
+
+__all__ = ['get_logger', 'init_logging', 'update_logging', 'LoggerMixin']
--- savant/utils/log/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Logging utils package."""
-
-from .log_setup import get_logger, init_logging, update_logging
-from .logger_mixin import LoggerMixin
