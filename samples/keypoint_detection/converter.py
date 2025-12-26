8fe0f77d2523d33e74c4617b42c7686b28715d7c
Ivan A. Kudriavtsev <kudryavtsev_ia@bw-sw.com>
2025-12-26 15:39:49 +0100

    Keypoint detector demo upgraded to use YOLO11 Pose.

--- samples/keypoint_detection/converter.py
+++ samples/keypoint_detection/converter.py
@@ -9,8 +9,8 @@ from savant.base.model import ComplexModel
 from savant.utils.nms import nms_cpu
 
 
-class YoloV8PoseConverter(BaseComplexModelOutputConverter):
-    """`YOLOv8Pose converter."""
+class Yolo11PoseConverter(BaseComplexModelOutputConverter):
+    """`YOLOv11Pose converter. Also known to work for YOLOv8Pose."""
 
     def __init__(
         self,
