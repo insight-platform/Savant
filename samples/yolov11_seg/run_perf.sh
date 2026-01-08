a0bdb3fcc5652157b632e5bdf60f915f6725da9c
Den Medyantsev <denisvmedyantsev@gmail.com>
2026-01-08 11:13:13 +0100

    Fix seg sample perf script.

--- samples/yolov11_seg/run_perf.sh
+++ samples/yolov11_seg/run_perf.sh
@@ -6,15 +6,8 @@
 # The YQ_ARGS are module configuration updates in yq notation,
 # e.g. ".parameters.batch_size=4".
 
-MODULE_CONFIG=samples/yolov8_seg/module/module.yml
+MODULE_CONFIG=samples/yolov11_seg/module/module.yml
 DATA_LOCATION=data/shuffle_dance.mp4
 
 source samples/assets/run_perf_helper.sh
-
-if [ "$(uname -m)" = "aarch64" ]; then
-  YQ_ARGS+=('.parameters.model_output_converter="converter"')
-else
-  YQ_ARGS+=('.parameters.model_output_converter="gpu_converter"')
-fi
-
 run_perf $MODULE_CONFIG $DATA_LOCATION
