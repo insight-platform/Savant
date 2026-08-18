cc84e7c32179388cb7a135298f340e8fda3ce71f
Oleg Abramov <abramov.o.v@gmail.com>
2026-08-18 22:04:51 +0700

    Fix quotes after expansion

--- samples/assets/run_perf_helper.sh
+++ samples/assets/run_perf_helper.sh
@@ -92,6 +92,10 @@ function run_perf {
 
   config_perf "$module_config" "$perf_config" "${YQ_ARGS[@]}"
 
-  LOGLEVEL=info ./scripts/run_module.py \
-    ${DOCKER_IMAGE:+--docker-image "$DOCKER_IMAGE"} "$perf_config"
+  local module_args=()
+  if [ -n "${DOCKER_IMAGE-}" ]; then
+    module_args+=(--docker-image "$DOCKER_IMAGE")
+  fi
+
+  LOGLEVEL=info ./scripts/run_module.py "${module_args[@]}" "$perf_config"
 }
